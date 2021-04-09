74e340b268f0ed09a289ee635528fc8b2c51a496
Dhita Irma <dhita.irma@gmail.com>
2021-04-09 19:30:01 +0800

    Start implementation of The Burger Builder Container
    Create BurgerBuilder container and use it in App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Layout from './components/Layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
 class App extends Component {
   state = {  }
@@ -7,6 +8,7 @@ class App extends Component {
     return ( 
       <div>
         <Layout>
+          <BurgerBuilder />
         </Layout>
       </div>
      );
---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import Aux from "../../hoc/Auxiliary";
+
+class BurgerBuilder extends Component {
+  state = {};
+  render() {
+    return (
+      <Aux>
+        <div>Burger</div>
+        <div>Build Control</div>
+      </Aux>
+    );
+  }
+}
+
+export default BurgerBuilder;
