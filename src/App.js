088e6f76abee9fddb5e32808ba8230ae93fc7bdc
Dhita Irma <dhita.irma@gmail.com>
2021-04-09 19:28:43 +0800

    Create Layout component
    Setup Layout.js, Layout.module.css, and create Auxiliary.js file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import Layout from './components/Layout/Layout';
 
 class App extends Component {
   state = {  }
   render() { 
     return ( 
       <div>
-
+        <Layout>
+        </Layout>
       </div>
      );
   }
