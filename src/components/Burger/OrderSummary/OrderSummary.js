0e6eecb3d397e0e5984069d2995e93a9188077ac
Dhita Irma <dhita.irma@gmail.com>
2021-04-23 15:36:49 +0800

    Improve performance
    Only update Order Summary when Modal is shown

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,31 +1,34 @@
-import React from "react";
+import React, { Component } from 'react';
 
 import Aux from "../../../hoc/Auxiliary";
 import Button from "../../UI/Button/Button";
 
-const orderSummary = (props) => {
-  const ingredientSummary = Object.keys(props.ingredients).map((igKey) => (
-    <li key={igKey}>
-      <span style={{ textTransform: "capitalize" }}>{igKey}</span>:
-      {props.ingredients[igKey]}
-    </li>
-  ));
+class OrderSummary extends Component {
 
-  return (
-    <Aux>
+  render() {
+    const ingredientSummary = Object.keys(this.props.ingredients).map((igKey) => (
+      <li key={igKey}>
+        <span style={{ textTransform: "capitalize" }}>{igKey}</span>:
+        {this.props.ingredients[igKey]}
+      </li>
+    ));
+
+    return (
+      <Aux>
       <h3>Your Order</h3>
       <p>A delicious burger with the following ingredients:</p>
       <ul>{ingredientSummary}</ul>
-      <p><strong>Total Price: $ {props.price.toFixed(2)}</strong></p>
+      <p><strong>Total Price: $ {this.props.price.toFixed(2)}</strong></p>
       <p>Continue to Checkout?</p>
-      <Button btnType="Danger" clicked={props.purchaseCancelled}>
+      <Button btnType="Danger" clicked={this.props.purchaseCancelled}>
         CANCEL ORDER
       </Button>
-      <Button btnType="Success" clicked={props.purchaseContinued}>
+      <Button btnType="Success" clicked={this.props.purchaseContinued}>
         CONTINUE
       </Button>
     </Aux>
-  );
-};
+    );
+  }
+}
 
-export default orderSummary;
+export default OrderSummary;
